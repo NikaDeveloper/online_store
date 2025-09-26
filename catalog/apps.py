e58f2578c99510b1e0b52bbbca109621a6e43fd2
Nika Alexandrovna <megrelynika@gmail.com>
2025-09-27 03:31:10 +0400

    realizing online store

--- catalog/apps.py
+++ catalog/apps.py
@@ -5,7 +5,3 @@ class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalog'
 
-
-class ContactsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contacts'
--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,15 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print(f'Пользователь {name}, Телефон: {phone}, Сообщение: {message}')
+
+        context = {'success_message': 'Ваше сообщение успешно отправлено!'}
+        return render(request, 'contacts.html', context)
+
     return render(request, 'contacts.html')
+
