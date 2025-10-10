af50371f1764fedc3ca0f817a5ef74b162b4fd0b
Nika Alexandrovna <megrelynika@gmail.com>
2025-10-10 17:41:40 +0400

    Добавлены изображения для товаров

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = CatalogConfig.name
@@ -13,3 +15,6 @@ urlpatterns = [
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('product/create/', views.create_product, name='create_product'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
