4f705a5f1b980ba871d249a645ab675191556710
Nika Alexandrovna <megrelynika@gmail.com>
2025-10-12 20:28:57 +0400

    paging error fixed

--- catalog/views.py
+++ catalog/views.py
@@ -7,8 +7,8 @@ class ProductListView(ListView):
     model = Product
     template_name = 'catalog/home.html'
     context_object_name = 'products'
-
     paginate_by = 9
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
