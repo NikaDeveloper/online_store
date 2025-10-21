26942f391af319e97c1c7522b90ffcbc6d916a19
Nika Alexandrovna <megrelynika@gmail.com>
2025-10-21 22:22:52 +0400

    implementation of authentication, registration, authorization, and access restriction

--- catalog/urls.py
+++ catalog/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
-    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='delete'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='delete_product'),
 ]
--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django_countries.fields import CountryField
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True,)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name='Телефон')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='Аватар')
+    country = CountryField(blank=True, null=True, verbose_name='Страна')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', ]
